3cd286b8ebd5eb6555a6fa291edd7a9dc5c02639
0101 <ldshuang@gmail.com>
2018-01-31 10:58:05 +0800

    chore: add deps

--- mix.exs
+++ mix.exs
@@ -23,6 +23,9 @@ defmodule TgWebhooksBot.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:nadia, "~> 0.4.2"},
+      {:diplomat, "~> 0.2"},
+      {:distillery, "~> 1.5", runtime: false}
     ]
   end
 end
