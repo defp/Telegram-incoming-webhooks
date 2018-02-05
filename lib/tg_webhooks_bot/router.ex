7a67799fe68d478a208ad5a14b544ce7282d3d21
0101 <ldshuang@gmail.com>
2018-02-05 17:40:43 +0800

    feat: set webhook url

--- lib/tg_webhooks_bot/router.ex
+++ lib/tg_webhooks_bot/router.ex
@@ -15,6 +15,16 @@ defmodule TgWebhooksBot.Router do
     send_resp(conn, 200, "ok")
   end
 
+  get "/set_webhook" do
+    webhook_url = Application.get_env(:tg_webhooks_bot, :webhook_url)
+    case Nadia.set_webhook(url: webhook_url) do
+      :ok ->
+        send_resp(conn, 200, "ok")
+      {:error, %Nadia.Model.Error{reason: reason}} -> 
+        send_resp(conn, 400, Atom.to_string(reason))
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
