7ce0f025992b32cd63beaf5e17753af3e39d9852
0101 <ldshuang@gmail.com>
2018-02-06 00:24:34 +0800

    feat: handle ping msg

--- lib/tg_webhooks_bot/router.ex
+++ lib/tg_webhooks_bot/router.ex
@@ -1,14 +1,13 @@
 defmodule TgWebhooksBot.Router do
+  require Logger
   use Plug.Router
 
   plug Plug.Logger
   plug Plug.Parsers, parsers: [:urlencoded, :json],
                    pass:  ["text/*"],
                    json_decoder: Poison
-
   plug :match
   plug :dispatch
-
 
   # https://api.slack.com/incoming-webhooks
   post "/incoming" do
@@ -25,7 +24,30 @@ defmodule TgWebhooksBot.Router do
     end
   end
 
+  post "/cmd" do
+    params = conn.params
+    handle(params["message"]["text"], params["message"])
+    Logger.debug("receive: ", params["message"]["text"])
+    send_resp(conn, 200, "ok")
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
+  end
+
+  # handle cmd
+  defp handle("/start", message) do
+    chat_id = message["chat"]["id"]
+    Nadia.send_message(chat_id, "TODO")
+  end
+
+  defp handle("/ping", message) do
+    chat_id = message["chat"]["id"]
+    Nadia.send_message(chat_id, "pong")
+  end
+
+  defp handle(_, message) do
+    chat_id = message["chat"]["id"]
+    Nadia.send_message(chat_id, "error")
   end
 end
