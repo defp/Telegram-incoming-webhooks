52afa8b487fb6a8df4409ec94809c7701df66cf6
ld <ldshuang@gmail.com>
2018-04-26 01:08:38 +0800

    fix: set schema http

--- lib/tg_webhooks_bot/application.ex
+++ lib/tg_webhooks_bot/application.ex
@@ -8,15 +8,8 @@ defmodule TgWebhooksBot.Application do
     # List all child processes to be supervised
     cowboy_options = Application.get_env(:tg_webhooks_bot, :cowboy_options) || [port: 5000]
 
-    schema =
-      if Mix.env() == :dev do
-        :http
-      else
-        :https
-      end
-
     children = [
-      Plug.Adapters.Cowboy.child_spec(schema, TgWebhooksBot.Router, [], cowboy_options)
+      Plug.Adapters.Cowboy.child_spec(:http, TgWebhooksBot.Router, [], cowboy_options)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
