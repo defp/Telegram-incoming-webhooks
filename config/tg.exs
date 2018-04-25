b92c6d148e9eae5beb1a4a3decd1783b85361a4a
ld <ldshuang@gmail.com>
2018-04-26 00:38:24 +0800

    fix: get config from system.env

---
+++ config/tg.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :nadia, token: System.get_env("TELEGRAM_TOKEN")
+
+config :tg_webhooks_bot,
+  cowboy_options: [
+    port: System.get_env("PORT")
+  ]
+
+config :tg_webhooks_bot, host_url: System.get_env("HOST_URL")
