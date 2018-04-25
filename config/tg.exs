f2a4f0308d4b2d7c89120dcc022fc5135345f635
ld <ldshuang@gmail.com>
2018-04-26 01:06:07 +0800

    fix: port to integer

--- config/tg.exs
+++ config/tg.exs
@@ -4,7 +4,7 @@ config :nadia, token: System.get_env("TELEGRAM_TOKEN")
 
 config :tg_webhooks_bot,
   cowboy_options: [
-    port: System.get_env("PORT")
+    port: System.get_env("PORT") |> String.to_integer
   ]
 
 config :tg_webhooks_bot, host_url: System.get_env("HOST_URL")
