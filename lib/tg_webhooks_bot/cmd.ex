b9f737c934d28b846c24eec6759b8cc2db79a787
ld <ldshuang@gmail.com>
2018-04-26 14:15:50 +0800

    refactor: cmd msg

--- lib/tg_webhooks_bot/cmd.ex
+++ lib/tg_webhooks_bot/cmd.ex
@@ -1,24 +1,39 @@
 defmodule TgWebhooksBot.CMD  do
-   # handle cmd
+  require Logger
+
   def handle("/start", message) do
     chat_id = message["chat"]["id"]
-    host_url = Application.get_env(:tg_webhooks_bot, :host_url)
-    text = "hook url #{host_url}/incoming/#{chat_id}"
-    Nadia.send_message(chat_id, text)
+    msg = """
+    command:
+    /text - simple hook url with text params
+    """
+    send_message(chat_id, msg)
   end
 
   # callback_url@ form group
   def handle("/text" <> _, message) do
-    handle("/start", message)
-  end
-
-  def handle("/ping", message) do
     chat_id = message["chat"]["id"]
-    Nadia.send_message(chat_id, "pong")
+    host_url = Application.get_env(:tg_webhooks_bot, :host_url)
+    msg = """
+    hook url:
+    #{host_url}/incoming/#{chat_id}
+    example:
+    #{host_url}/incoming/#{chat_id}?text=helloworld
+    """
+    send_message(chat_id, msg)
   end
 
   def handle(_, message) do
-    chat_id = message["chat"]["id"]
-    Nadia.send_message(chat_id, "error")
+    send_message(message["chat"]["id"], "error command")
+  end
+
+  defp send_message(chat_id, msg) do
+    case Nadia.send_message(chat_id, msg) do
+      {:ok, _result} ->
+        :ok
+      {:error, %Nadia.Model.Error{reason: reason}} ->
+        Logger.error("send_message #{reason}")
+        :error
+    end
   end
 end
